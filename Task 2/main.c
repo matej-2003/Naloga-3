27cc66f63bb73de0a740536a9654492c68467d2f
matej-2003 <matej.kodermac1@gmail.com>
2023-01-06 14:33:27 +0100

    dodajanje fukcije, ki prever napacen vnos

--- Task 2/main.c
+++ Task 2/main.c
@@ -97,56 +97,60 @@ char slikice[10][16 * 8] = {
 };
 
 char* seznam_besed[] = {
-	"volitve", "poreklo", "enolončnica", "zadovoljivo", "naključno", "upornik", "strašljivo", "vbrizgati", "graciozen", "razpakirati", "velikan", "nadaljevati",
-	"ročaj", "smešno", "iskrena", "izjemen", "previden", "fingiran", "moški", "previden", "propad", "šepav", "spektakularno", "futuristično", "žilav", "prestol",
-	"roka", "potovanja", "mračno", "list", "čisto", "široko", "sodelovanje", "odbor", "fakulteta", "parlament", "sestava", "koalicija", "republika", "slovenija",
-	"prisega", "epidemija", "bolezen", "september", "najmanj", "madžarska", "zamenjati", "označeno", "pregledati", "pravilo", "strmo", "zmešana", "priznati"
+	"bolezen", "čisto", "enolončnica", "epidemija", "fakulteta", "fingiran", "futuristično", "graciozen", "iskrena", "izjemen", "koalicija", "list",
+	"madžarska", "moški", "mračno", "nadaljevati", "najmanj", "naključno", "odbor", "označeno", "parlament", "poreklo", "potovanja", "pravilo",
+	"pregledati", "prestol", "previden", "previden", "prisega", "priznat", "propad", "razpakirati", "republika", "ročaj", "roka", "šepav",
+	"september", "sestava", "široko", "slovenija", "smešno", "sodelovanje", "spektakularno", "strašljivo", "strmo", "upornik", "vbrizgati",
+	"velikan", "volitve", "zadovoljivo", "zamenjati", "žilav", "zmešana", 
 };
 
-bool izpolni_besedo(char* skrita_beseda, char* beseda, char crka);
-void izpisi_besedo(char* skrita_beseda);
+bool izpolni_besedo(char skrita_beseda[], char beseda[], char crka);
+void izpisi_besedo(char skrita_beseda[]);
 int str_len(char word[]);
+bool preveri_napacen_vnos(char napacni_vnosi[], char crka);
 
 int main() {
 	srand(time(NULL));
 
 	int index = rand() % (sizeof(seznam_besed) / sizeof(char*));
-	char* beseda = seznam_besed[0];
+	char* beseda = seznam_besed[index];
 	char* skrita_beseda = malloc(sizeof(char) * str_len(beseda));
-	for (int i=0; beseda[i]!='\0'; i++)
+	int i = 0;
+	for (;beseda[i]!='\0'; i++)
 		skrita_beseda[i] = '_';
+	skrita_beseda[i + 1] = '\0';
 	// strcpy(skrita_beseda, beseda);
 	int stevilo_napacnih_vnosov = 0;
 	char napacni_vnosi[127];
-
-
-	char odgovor[10];
 	
 	do {
+		char odgovor[10];
 		char c;
 
 		printf("Iskana beseda: ");
 		izpisi_besedo(skrita_beseda);
 		printf("\n");
-		printf("Napacni vnosti: %s\n", napacni_vnosi);
+		printf("Napacni vnosi: %s\n", napacni_vnosi);
 
 		printf("%s\n\n", slikice[stevilo_napacnih_vnosov]);
 		
 		printf("Vnesi crko: ");
 		fgets(odgovor, 10, stdin);
-		if (sscanf(odgovor, "%c", &c) != 1) {
-			continue;
-		}
 		fflush(stdin);
+		if (sscanf(odgovor, "%c", &c) != 1) {
+			break;
+		}
 		
-		if (izpolni_besedo(skrita_beseda, beseda, c) == false) {
-			stevilo_napacnih_vnosov++;
+		if (izpolni_besedo(skrita_beseda, beseda, c) == false && preveri_napacen_vnos(napacni_vnosi, c)) {
+			printf("%i, %s\n", stevilo_napacnih_vnosov, napacni_vnosi);
 			napacni_vnosi[stevilo_napacnih_vnosov] = c;
+			napacni_vnosi[stevilo_napacnih_vnosov + 1] = '\0';
+			stevilo_napacnih_vnosov++;
 		}
 	} while (stevilo_napacnih_vnosov < 10 && strcmp(beseda, skrita_beseda) != 0);
 
 	printf("Iskana beseda: ");
-	izpisi_besedo(skrita_beseda);
+	izpisi_besedo(beseda);
 	printf("\n");
 
 	// while (1) {
@@ -161,7 +165,7 @@ int main() {
 }
 
 
-bool izpolni_besedo(char* skrita_beseda, char* beseda, char crka) {
+bool izpolni_besedo(char skrita_beseda[], char beseda[], char crka) {
 	bool vsebuje = false;
 	for (int i=0; beseda[i] != '\0'; i++) {
 		if (beseda[i] == crka) {
@@ -170,6 +174,13 @@ bool izpolni_besedo(char* skrita_beseda, char* beseda, char crka) {
 		}
 	}
 	return vsebuje;
+}
+
+bool preveri_napacen_vnos(char napacni_vnosi[], char crka) {
+	for (int i=0; napacni_vnosi[i] != '\0'; i++)
+		if (napacni_vnosi[i] == crka)	
+			return false;
+	return true;
 }
 
 void izpisi_besedo(char* skrita_beseda) {
